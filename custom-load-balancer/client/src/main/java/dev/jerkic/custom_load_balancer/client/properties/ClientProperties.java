f473e35a6e04fb9a940fc64f7f9329cc2daa8be7
Marko Jerkić <markojerkic266@gmail.com>
2024-12-27 13:38:59 +0100

    can't run

--- custom-load-balancer/client/src/main/java/dev/jerkic/custom_load_balancer/client/properties/ClientProperties.java
+++ custom-load-balancer/client/src/main/java/dev/jerkic/custom_load_balancer/client/properties/ClientProperties.java
@@ -1,13 +1,15 @@
 package dev.jerkic.custom_load_balancer.client.properties;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Data
+@Getter
 @ConfigurationProperties(prefix = "discovery.client")
 @Configuration
+@AllArgsConstructor
 public class ClientProperties {
-  private final String discoveryServerUrl;
-  private final String serviceName;
+  private String discoveryServerUrl;
+  private String serviceName;
 }
--- custom-load-balancer/examples/example-server-1/src/main/java/dev/jerkic/custom_load_balancer/example_server_1/ExampleServer1Application.java
+++ custom-load-balancer/examples/example-server-1/src/main/java/dev/jerkic/custom_load_balancer/example_server_1/ExampleServer1Application.java
@@ -3,11 +3,10 @@ package dev.jerkic.custom_load_balancer.example_server_1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "dev.jerkic.custom_load_balancer.client")
 public class ExampleServer1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExampleServer1Application.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ExampleServer1Application.class, args);
+  }
 }
