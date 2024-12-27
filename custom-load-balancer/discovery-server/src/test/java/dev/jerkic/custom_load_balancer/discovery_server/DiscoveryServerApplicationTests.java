3a1a4d1f3c15fa70f656ee857c757af99eb0cc8f
Marko Jerkić <markojerkic266@gmail.com>
2024-12-27 10:15:01 +0100

    start setup

---
+++ custom-load-balancer/discovery-server/src/test/java/dev/jerkic/custom_load_balancer/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jerkic.custom_load_balancer.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
