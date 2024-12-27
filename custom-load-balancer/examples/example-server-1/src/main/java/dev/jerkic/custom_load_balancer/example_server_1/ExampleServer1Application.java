b4e1315deb743afe6211b3033819ace946b7153f
Marko Jerkić <markojerkic266@gmail.com>
2024-12-27 12:54:30 +0100

    example server 1

---
+++ custom-load-balancer/examples/example-server-1/src/main/java/dev/jerkic/custom_load_balancer/example_server_1/ExampleServer1Application.java
@@ -0,0 +1,13 @@
+package dev.jerkic.custom_load_balancer.example_server_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleServer1Application.class, args);
+	}
+
+}
---
+++ custom-load-balancer/examples/example-server-1/src/test/java/dev/jerkic/custom_load_balancer/example_server_1/ExampleServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jerkic.custom_load_balancer.example_server_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
