12aa57e2f640e5cfc8ae3de5c1d89e2e1d7f51a3
Marko Jerkić <markojerkic266@gmail.com>
2024-12-27 12:09:47 +0100

    client service

--- custom-load-balancer/client/src/main/java/dev/jerkic/custom_load_balancer/client/service/ClientHealthService.java
+++ custom-load-balancer/client/src/main/java/dev/jerkic/custom_load_balancer/client/service/ClientHealthService.java
@@ -4,25 +4,35 @@ import dev.jerkic.custom_load_balancer.client.properties.ClientProperties;
 import dev.jerkic.custom_load_balancer.shared.model.dto.HealthUpdateInput;
 import dev.jerkic.custom_load_balancer.shared.model.dto.RegisterInput;
 import dev.jerkic.custom_load_balancer.shared.service.ServiceHealthService;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @EnableConfigurationProperties(ClientProperties.class)
 @RequiredArgsConstructor
 public class ClientHealthService implements ServiceHealthService {
   private final ClientProperties clientProperties;
+  private final RestTemplate restTemplate;
+  private final AtomicReference<String> serviceId = new AtomicReference<>();
 
   @Override
   public String registerService(RegisterInput registerInput) {
-    // TODO Auto-generated method stub
-    return null;
+    var serviceId =
+        this.restTemplate.postForObject(
+            this.clientProperties.getDiscoveryServerUrl() + "/register",
+            registerInput,
+            String.class);
+
+    this.serviceId.set(serviceId);
+    return serviceId;
   }
 
   @Override
   public void updateHealth(HealthUpdateInput healthUpdateInput) {
-    // TODO Auto-generated method stub
-
+    this.restTemplate.postForObject(
+        this.clientProperties.getDiscoveryServerUrl() + "/health", healthUpdateInput, Void.class);
   }
 }
