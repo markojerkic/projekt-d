05b6efb9f669fa790b40b6ad159530000f05e677
Marko Jerkić <markojerkic266@gmail.com>
2024-12-28 23:34:39 +0100

    start tests

---
+++ custom-load-balancer/discovery-server/src/test/java/dev/jerkic/custom_load_balancer/discovery_server/ServiceManagementTests.java
@@ -0,0 +1,6 @@
+package dev.jerkic.custom_load_balancer.discovery_server;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ServiceManagementTests {}
