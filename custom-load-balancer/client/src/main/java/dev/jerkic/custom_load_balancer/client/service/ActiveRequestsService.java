4998c7985f0ccd14172360f6b61f54fa161d08f4
Marko Jerkić <markojerkic266@gmail.com>
2024-12-31 23:39:01 +0100

    filter works

--- custom-load-balancer/client/src/main/java/dev/jerkic/custom_load_balancer/client/service/ActiveRequestsService.java
+++ custom-load-balancer/client/src/main/java/dev/jerkic/custom_load_balancer/client/service/ActiveRequestsService.java
@@ -1,18 +1,22 @@
 package dev.jerkic.custom_load_balancer.client.service;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ActiveRequestsService {
   private AtomicInteger activeRequests = new AtomicInteger(0);
 
   public void incrementActiveRequests() {
-    this.activeRequests.incrementAndGet();
+    var curr = this.activeRequests.incrementAndGet();
+    log.info("Incremented active requests. Current number of requests: {}", curr);
   }
 
   public void decrementActiveRequests() {
-    this.activeRequests.decrementAndGet();
+    var curr = this.activeRequests.decrementAndGet();
+    log.info("Decremented active requests. Current number of requests: {}", curr);
   }
 
   public int getActiveRequests() {
