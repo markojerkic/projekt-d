1c3d0269d0ec8efb213ae2eab2b42e9c761ad57a
Marko Jerkić <markojerkic266@gmail.com>
2024-12-31 21:39:20 +0100

    watch is active

--- custom-load-balancer/discovery-server/src/main/resources/static/js/index.js
+++ custom-load-balancer/discovery-server/src/main/resources/static/js/index.js
@@ -10,7 +10,36 @@ function updateTimes() {
 	}
 }
 
+function watchHtmxTest() {
+	htmx.on("htmx:afterSwap", function (event) {
+		/** @type {XMLHttpRequest} */
+		const responseXHR = event.detail.xhr;
+		const instanceId = getHeader("X-Lb-instance", responseXHR);
+
+		const sourceELementAttributes = event.detail.elt?.attributes;
+
+		const elementName =
+			sourceELementAttributes && sourceELementAttributes["data-name"]?.value;
+
+		if (elementName) {
+			console.log("HTMX has swapped elements", elementName);
+			console.log("From instance", instanceId);
+		} else {
+			console.warn("HTMX has swapped elements, but not from test fetcher");
+		}
+	});
+}
+
+function getHeader(name, responseXHR) {
+	const headers = responseXHR.getAllResponseHeaders();
+	const header = headers
+		.split("\n")
+		.filter((header) => header.includes(name.toLowerCase()))
+		.join("");
+	return header;
+}
+
 window.addEventListener("load", function () {
-	alert("Hello, World!");
 	updateTimes();
+	watchHtmxTest();
 });
